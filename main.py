02bc3f699104edbde412159efcd164a96d87b736
vidyesh <vidyesh95@gmail.com>
2021-10-02 13:23:05 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# Python3 program to add two numbers
+
+# Ask user for first number
+number1 = input("Enter the first number: ")
+# Ask user for second number
+number2 = input("Enter the second number: ")
+
+# Adding two floating point numbers
+sumOfNumbers: float = float(number1) + float(number2)
+
+# Print the sum of two numbers
+print("Sum of {0} and {1} is {2}".format(number1, number2, sumOfNumbers))
